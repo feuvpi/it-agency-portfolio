d57ef6af5ff3913a315883839261d4c533e8cc69
feuvpi <fredvpg@gmail.com>
2022-09-27 00:24:02 -0300

    initial update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,24 +3,23 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#2a0955]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, my name is</p>
+        <p className='text-pink-600'>IT Agency</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          Clint Briley
+          Dende Tech
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Full Stack Developer.
+        <h2 className='text-4xl sm:text-5xl font-bold text-[#8892b0]'>
+          We build reliable Software.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+          We're a team of full-stack software developers that specializes in software design and implementation.
+
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
+            PORTFOLIO
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,16 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
+import { GiPeanut } from 'react-icons/gi'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#340b69] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+        <GiPeanut size={50} />
       </div>
 
       {/* menu */}
@@ -36,7 +37,7 @@ const Navbar = () => {
         </li>
         <li>
           <Link to='skills' smooth={true} duration={500}>
-            Skills
+            Tech
           </Link>
         </li>
         <li>
